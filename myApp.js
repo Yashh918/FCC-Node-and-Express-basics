2a5b5a1624e7c3cbea34ebf29570914220c988df
Yashh918 <yashdhopeshwarkar@gmail.com>
2024-08-16 00:32:57 +0530

    basic node and express

--- myApp.js
+++ myApp.js
@@ -1,39 +1,29 @@
-let express = require('express');
+let express = require("express");
+require('dotenv').config()
+
 let app = express();
+app.use((req, res, next) => {
+    console.log(req.method + " " + req.path + " - " + req.path)
+    next()
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+})
+
+app.use('/public', express.static(__dirname + '/public'))
+
+let response
+app.get("/json", (req, res) => {
+    if (process.env.MESSAGE_STYLE === "allCaps") {
+        response = "Hello json".toUpperCase();
+    } else {
+        response = "Hello World";
+    }
+    return res.json({"message": response})
+})
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
