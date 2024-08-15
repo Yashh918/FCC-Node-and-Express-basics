0752de8a9a0e417e15f5ac9b43989d1a8884ceaa
Yashh918 <yashdhopeshwarkar@gmail.com>
2024-08-16 01:55:23 +0530

    completed the basics of Node and Express

--- myApp.js
+++ myApp.js
@@ -1,11 +1,13 @@
+const bodyParser = require("body-parser");
 let express = require("express");
 require('dotenv').config()
 
 let app = express();
 app.use((req, res, next) => {
-    console.log(req.method + " " + req.path + " - " + req.path)
+    console.log(req.method + " " + req.path + " - " + req.ip)
     next()
 })
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
@@ -20,10 +22,30 @@ app.get("/json", (req, res) => {
     } else {
         response = "Hello World";
     }
-    return res.json({"message": response})
+    res.json({"message": response})
 })
 
+const time = (req, res, next) => {
+    req.time = new Date().toString()
+    next()
+}
+app.get('/now', time , (req, res) => {
+   res.send(req.time)
+})
 
+app.get('/:word/echo', (req, res) => {
+    const {word} = req.params
+    res.json({echo: word})
+})
 
+app.get('/name', (req, res) => {
+    const {first, last} = req.query
+    res.json({name: first + " " + last})
+})
+
+app.post('/name', (req, res) => {
+    const {first, last} = req.body
+    res.json({name: first + " " + last})
+})
 
 module.exports = app;
